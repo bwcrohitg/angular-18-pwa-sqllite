6f5703dc17c5fc185a79a3d47e716e99c104c12d
Rohit Gadhave <rohit.gadhave@brainwaveconsulting.co.in>
2024-07-08 18:37:08 +0530

    router nev

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'tab1',
+    loadComponent: () =>
+      import('./tab-1/tab-1.component').then((m) => m.Tab1Component),
+  },
+  {
+    path: 'tab2',
+    loadComponent: () =>
+      import('./tab-2/tab-2.component').then((m) => m.Tab2Component),
+  },
+  {
+    path: 'tab3',
+    loadComponent: () =>
+      import('./tab-3/tab-3.component').then((m) => m.Tab3Component),
+  },
+];
