705f7515cbf9cc4d3c325c3dc47e7d99837b3382
Rohit Gadhave <rohit.gadhave@brainwaveconsulting.co.in>
2024-07-09 15:15:09 +0530

    env set

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -6,8 +6,8 @@ import { environment } from '../../environments/environment.development';
   providedIn: 'root',
 })
 export class HttpService {
-  // private http = inject(HttpClient);
-  constructor(public http: HttpClient) {}
+  private http = inject(HttpClient);
+  constructor() {}
   getData(tab: string): Promise<any> {
     const res = this.http.get(environment.url + tab);
     return new Promise((reserved, reject) => {
